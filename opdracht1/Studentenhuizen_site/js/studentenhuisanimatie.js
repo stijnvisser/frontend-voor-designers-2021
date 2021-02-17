57032a927c541998d0638fe2880bddb9c9ddf7f7
stijnvisser <70750931+stijnvisser@users.noreply.github.com>
2021-02-17 12:38:20 +0100

    Update studentenhuisanimatie.js

--- opdracht1/Studentenhuizen_site/js/studentenhuisanimatie.js
+++ opdracht1/Studentenhuizen_site/js/studentenhuisanimatie.js
@@ -22,20 +22,77 @@ window.onscroll = function () {
 
 /*bron: https://www.w3schools.com */
 
-var openMenu = document.querySelector('#openbtn');
-var closeMenu = document.querySelector('#closebtn');
+var openMenu1 = document.querySelector('#openbtn');
+var closeMenu1 = document.querySelector('#closebtn');
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+var openMenu2 = document.querySelector('#openbtn');
+var closeMenu2 = document.querySelector('#closebtn');
+
+var openMenu3 = document.querySelector('#openbtn');
+var closeMenu3 = document.querySelector('#closebtn');
+
+var openMenu4 = document.querySelector('#openbtn');
+var closeMenu4 = document.querySelector('#closebtn');
+
+function openNav1() {
+    document.getElementById("mySidenav1").style.width = "250px";
     document.querySelector(".main").style.marginRight = "250px";
     document.body.style.boxShadow = "rgba(0,0,0,0.4)";
 }
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+function closeNav1() {
+    document.getElementById("mySidenav1").style.width = "0";
     document.querySelector(".main").style.marginRight = "0";
     document.body.style.boxShadow = "white";
 }
 
-openMenu.addEventListener("click", openNav);
-closeMenu.addEventListener("click", closeNav);
+function openNav2() {
+    document.getElementById("mySidenav2").style.width = "250px";
+    document.querySelector(".main").style.marginRight = "250px";
+    document.body.style.boxShadow = "rgba(0,0,0,0.4)";
+}
+
+function closeNav2() {
+    document.getElementById("mySidenav2").style.width = "0";
+    document.querySelector(".main").style.marginRight = "0";
+    document.body.style.boxShadow = "white";
+}
+
+function openNav3() {
+    document.getElementById("mySidenav3").style.width = "250px";
+    document.querySelector(".main").style.marginRight = "250px";
+    document.body.style.boxShadow = "rgba(0,0,0,0.4)";
+}
+
+function closeNav3() {
+    document.getElementById("mySidenav3").style.width = "0";
+    document.querySelector(".main").style.marginRight = "0";
+    document.body.style.boxShadow = "white";
+}
+
+function openNav4() {
+    document.getElementById("mySidenav4").style.width = "250px";
+    document.querySelector(".main").style.marginRight = "250px";
+    document.body.style.boxShadow = "rgba(0,0,0,0.4)";
+}
+
+function closeNav4() {
+    document.getElementById("mySidenav4").style.width = "0";
+    document.querySelector(".main").style.marginRight = "0";
+    document.body.style.boxShadow = "white";
+}
+
+
+
+openMenu1.addEventListener("click", openNav1);
+closeMenu1.addEventListener("click", closeNav1);
+
+openMenu2.addEventListener("click", openNav2);
+closeMenu2.addEventListener("click", closeNav2);
+
+openMenu3.addEventListener("click", openNav3);
+closeMenu3.addEventListener("click", closeNav3);
+
+openMenu4.addEventListener("click", openNav4);
+closeMenu4.addEventListener("click", closeNav4);
+
