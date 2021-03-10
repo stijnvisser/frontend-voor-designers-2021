c89a7809a391c283068e7ad584098982ebae9700
stijnvisser <70750931+stijnvisser@users.noreply.github.com>
2021-03-10 14:37:55 +0100

    Update script.js

--- opdracht2/demo/_js/script.js
+++ opdracht2/demo/_js/script.js
@@ -4,43 +4,10 @@
 
 console.log('bladeren js');
 
-var fotos = ['image00.jpg','image01.jpg', 'image02.jpg', 'image03.jpg', 'image04.jpg', 'image05.jpg', 'image06.jpg'];
+    var fadeIn = document.getElementById('fadeIn1')
 
-var fotoImg = document.querySelector('#foto');
-var huidigNrSpan = document.querySelector('#huidigeFotoNr');
-var laatsteNrSpan = document.querySelector('#laatsteFotoNr');
-var vooruitButton = document.querySelector('#vooruit');
-var achteruitButton = document.querySelector('#terug');
-
-var huidigFotoNr = 0 ; // 
-toonHuidigeFoto();	
-
-function toonHuidigeFoto() {
-	console.log('bladeren ' + huidigFotoNr);
-	fotoImg.src = '_img/' + fotos[huidigFotoNr];
-	huidigNrSpan.textContent = huidigFotoNr ;
-	laatsteNrSpan.textContent = fotos.length - 1;
-
+function fadeOut() {
+    fadeIn.style.opacity = '0.5';
+    
 }
-
-
-
-function bladerVooruit() {
-	huidigFotoNr = huidigFotoNr + 1 ;
-	if ( huidigFotoNr > fotos.length -1 ) { 
-		huidigFotoNr = 1 ; 
-	}
-	toonHuidigeFoto();	
-}
-
-function bladerAchteruit() {
-	huidigFotoNr = huidigFotoNr - 1 ;
-	if ( huidigFotoNr < 1 ) { 
-		huidigFotoNr = fotos.length -1 ; 
-	}
-	toonHuidigeFoto();
-}
-
-vooruitButton.addEventListener('click' , bladerVooruit );
-vooruitButton.addEventListener('keypress' , bladerVooruit );
-achteruitButton.addEventListener('click' , bladerAchteruit );
+    fadeIn.addEventListener("click", fadeOut);
